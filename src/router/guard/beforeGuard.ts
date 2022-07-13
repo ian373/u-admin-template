5580d5243478642d8c31d0fa849aa54dffae8c6e
Ian171 <yxcwl10001@163.com>
2022-07-13 16:59:38 +0800

    perf(beforeGuard): 优化了前置路由的逻辑

--- src/router/guard/beforeGuard.ts
+++ src/router/guard/beforeGuard.ts
@@ -5,25 +5,31 @@ import { useAppStore } from "@/store/app";
 import { permissionHandle } from "@/utils/routes/permission";
 
 export function createBeforeGuard(router: Router) {
-  router.beforeEach(async (to, _from) => {
+  router.beforeEach(async (to, from) => {
     // 是否每次guard都要访问store?
-    const userStore = useUserStore();
     const appStore = useAppStore();
-    // 调试
+
     // console.log("beforeGuard: ", from.path, " => ", to.path);
 
-    // 鉴权
-    if (userStore.token) {
-      if (to.path === "/login") {
-        return "/dashboard";
-      }
-    } else {
-      const success = await permissionHandle();
-      if (success) {
-        return to.path;
+    // 刷新或首次访问网站，from.path === '/'
+    if (from.path === "/") {
+      // 鉴权
+      const userStore = useUserStore();
+      if (userStore.token) {
+        if (to.path === "/login") {
+          return "/dashboard";
+        }
+        // 这里什么也不做，放行路由，不能return to.path, 否则会无限重定向
       } else {
-        if (to.path !== "/login") {
-          return "/login";
+        const success = await permissionHandle();
+        if (success) {
+          // 只要return了一个路由地址，那么就会触发新的导航
+          // 因为permissionHandle()中添加了动态路由，如果这里的to.path是动态路由，必须触发一次新的导航
+          return to.path;
+        } else {
+          if (to.path !== "/login") {
+            return "/login";
+          }
         }
       }
     }
