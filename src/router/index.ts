c40792fb175ed2220dd545cb41de5569e5331529
Ian171 <yxcwl10001@163.com>
2022-07-19 20:13:51 +0800

    feat(*): 通过配置，可以把项目部署在服务器子目录下

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,16 @@ import { createRouter, createWebHistory } from "vue-router";
 import constantRoutes from "./routes/constRoutes";
 import { setupRouterGuard } from "./guard";
 
+import { getRouterBaseUrl } from "@/utils/routes/router_base_url";
+
+const baseUrl = getRouterBaseUrl();
+
 const routes = constantRoutes;
 
 const router = createRouter({
   // 使用electron时改为hash模式
   // history: createWebHashHistory()
-  history: createWebHistory(),
+  history: createWebHistory(baseUrl), //如果项目部署在服务器子目录上，需要指定前置路径
   routes,
 });
 
---
+++ src/utils/routes/router_base_url.ts
@@ -0,0 +1,11 @@
+export function getRouterBaseUrl() {
+  const url = import.meta.env.VITE_ROUTER_BASE_URL as string | undefined;
+
+  if (url) {
+    if (url.startsWith("/") && url.endsWith("/")) {
+      return url;
+    }
+  }
+
+  return "";
+}
